1489dd32d17c66c7c9a2ed85a7627316a1577ef4
XiaZhengXin <xzx@xiazhengxin.name>
2019-05-13 10:49:10 +0800

    use sqlite not mysql...
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- models/base.go
+++ models/base.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/sharljimhtsin/ipapk-server-fixed-pkg-error/conf"
 )
 
@@ -11,7 +12,7 @@ var orm *gorm.DB
 
 func InitDB() error {
 	var err error
-	orm, err = gorm.Open("mysql", conf.AppConfig.Database)
+	orm, err = gorm.Open("sqlite", conf.AppConfig.Database)
 	if err != nil {
 		return err
 	}
